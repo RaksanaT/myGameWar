3c9265396c2cb7f4c0e3830b66e5494e2d72c1bf
RoxannaTp <roxanataptygina@yahoo.com>
2022-11-12 15:36:23 -0500

    THATSIT

---
+++ war_test.js
@@ -0,0 +1,13 @@
+var expect = chai.expect;
+
+describe ('Suffle function', function(){
+    describe('#shuffleDeck', function(){
+        it ('should shuffle the deck randomly', function(){
+            var x = shuffleDeck;
+            expect(x).to.equal(Math.random);
+        });
+
+        // LIKE WHAT? THAT'S IT, I DON'T KNOW, I BROKE MY BRAIN.
+    });
+
+});
